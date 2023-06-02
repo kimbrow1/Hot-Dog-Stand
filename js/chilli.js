abce99bfefee2871857edaa36a7a7300ab7c5d7b
Brian Kimbrow <bkimbrow25@gmail.com>
2023-06-01 22:07:08 -0500

    newupdates

--- js/chilli.js
+++ js/chilli.js
@@ -7,14 +7,14 @@ let saleTax = 5.2;
 
 let qty = prompt("How many Hot Dogs?");
 let price = salesPrice - (salesPrice * discount / 100);
-let totalPrice = (price * qty)
-let finalPrice = totalPrice + (totalPrice * saleTax / 100) 
+let totalPrice = (price * qty);
+let finalPrice = totalPrice + (totalPrice * saleTax / 100) ;
 
 alert (`The final price is ${finalPrice}`);
 
 let salesPageElm = document.getElementById('sale-page');
 
-let saleTaxElm = document.createElement('i')
+let saleTaxElm = document.createElement('i');
 saleTaxElm.textContent = saleTax;
 salesPageElm.appendChild(saleTaxElm);
 
